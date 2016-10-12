7198c833ac7f3d281989bd1e42de5183e70256d7
kevin <929118967@qq.com>
2016-10-12 11:36:13 +0800

    Initial Commit

---
+++ TestAVFoundation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAVFoundation
+//
+//  Created by devzkn on 12/10/2016.
+//  Copyright © 2016 DevKevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAVFoundation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestAVFoundation
+//
+//  Created by devzkn on 12/10/2016.
+//  Copyright © 2016 DevKevin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
